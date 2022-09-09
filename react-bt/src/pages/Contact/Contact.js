135f77b6f7474ec24574ddf8b902bba905c0ca29
Anik Ronjon <anik35-497@diu.edu.bd>
2022-09-09 14:54:27 +0600

    counter

--- react-bt/src/pages/Contact/Contact.js
+++ react-bt/src/pages/Contact/Contact.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Contact() {
+  const [number, setNumber] = useState(0)
+  const Increment = () => {
+    setNumber(number + 1);
+  }
+  const Decrement = () => {
+    setNumber(number - 1);
+  }
+  const PreviousState = () => {
+    setNumber((prev)=>prev+2)
+  }
   return (
-    <div>Contact</div>
+    <>
+    <h2>Number Counter</h2>
+    <h3>Current value: {number}</h3>
+    <button className="btn btn-warning" onClick={Increment}>Increment</button>
+    <button className="btn btn-warning" onClick={Decrement}>Decrement</button>
+    <button className="btn btn-warning" onClick={PreviousState}>IncrementBy2</button>
+    </>
   )
 }
